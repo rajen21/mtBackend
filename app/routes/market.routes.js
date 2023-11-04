3cce98c626f30c3c58e62593201df5b0dbed74f9
Rajan chavda <www.chavdarajan@gmail.com>
2023-11-04 22:10:47 +0530

    updates

--- app/routes/market.routes.js
+++ app/routes/market.routes.js
@@ -3,7 +3,7 @@ import { PatchMarketData } from "../controller/market.controller.js";
 
 export default (app) => {
     const router = Router();
-    router.patch("/:id", PatchMarketData);
+    router.patch("/", PatchMarketData);
     
     app.use("/market-data", router);
 }
