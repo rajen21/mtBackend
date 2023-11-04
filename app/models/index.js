5e27cee45fbc971e5beb964a967b2c95fa7b0909
Rajan chavda <www.chavdarajan@gmail.com>
2023-11-04 21:40:01 +0530

    added market data functionality

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import dbConfig from "../config/db.config.js";
 import user from "./user.model.js";
+import marketData from "./market.model.js";
 
-export default { mongoose, url: dbConfig.url, user: user(mongoose) };
+export default { mongoose, url: dbConfig.url, user: user(mongoose), marketData: marketData(mongoose) };
---
+++ app/models/market.model.js
@@ -0,0 +1,51 @@
+export default (mongoose) => {
+  const schema = mongoose.Schema({
+    time: {
+        type: String,
+    },
+    kalyan: {
+        type: String
+    },
+    kalyanMorning: {
+        type: String
+    },
+    kalyanNight: {
+        type: String
+    },
+    milanDay: {
+        type: String
+    },
+    milanNight: {
+        type: String
+    },
+    rajdhaniDay: {
+        type: String
+    },
+    rajdhaniNight: {
+        type: String
+    },
+    mainBazar: {
+        type: String
+    },
+    milanBazarMorning: {
+        type: String
+    },
+    milanBazarDay: {
+        type: String
+    },
+    milanBazarNight: {
+        type: String
+    },
+    date: {
+        type: Date,
+        unique: true
+    }
+  });
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+  const market_data = mongoose.model("market_data", schema);
+  return market_data;
+};
