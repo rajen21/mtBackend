160aeea7a4c03b21c366b4b1226c4b075160bb59
Rajan chavda <www.chavdarajan@gmail.com>
2023-12-10 15:24:06 +0530

    added auth validation

--- app/routes/market.routes.js
+++ app/routes/market.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { PatchMarketData, getMarketData, getSpecificDateData } from "../controller/market.controller.js";
+import { jwtTokenValidate } from "../middleware/auth.middleware.js";
 
 export default (app) => {
     const router = Router();
-    router.post("/", PatchMarketData);
-    router.get("/", getMarketData);
-    router.post("/get-data", getSpecificDateData);
+    router.post("/", jwtTokenValidate, PatchMarketData);
+    router.get("/", jwtTokenValidate, getMarketData);
+    router.post("/get-data", jwtTokenValidate, getSpecificDateData);
     
     app.use("/market-data", router);
 }
