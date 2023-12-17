492310c213ee7f1a6d984916e7f20403316d1a12
Rajan chavda <www.chavdarajan@gmail.com>
2023-12-17 11:58:49 +0530

    added expiry of token

--- app/controller/login.controller.js
+++ app/controller/login.controller.js
@@ -19,7 +19,8 @@ export async function loginUser(req, res) {
 
     if (!active) {
       return res.status(403).send({
-        inactivity :"Your account is currently inactive. Please contact support for assistance."
+        inactivity:
+          "Your account is currently inactive. Please contact support for assistance.",
       });
     }
 
@@ -30,7 +31,8 @@ export async function loginUser(req, res) {
       agent = await User.findById(agentId);
       if (!agent.active) {
         return res.status(403).send({
-          inactivity: "The administrator account is currently inactive. Please contact support for assistance."
+          inactivity:
+            "The administrator account is currently inactive. Please contact support for assistance.",
         });
       }
     }
@@ -38,15 +40,37 @@ export async function loginUser(req, res) {
       admin = await User.findById(adminId);
       if (!admin.active) {
         return res.status(403).send({
-          inactivity: "The server is currently undergoing maintenance. We apologize for any inconvenience. Please try again later."
+          inactivity:
+            "The server is currently undergoing maintenance. We apologize for any inconvenience. Please try again later.",
         });
       }
     }
 
     const isPasswordValid = await bcrypt.compare(req.body.password, password);
     if (isPasswordValid) {
-      const token = jwt.sign({ id: _id, user_name, password, adminId, agentId, role, active, balance }, "secret123");
-      return res.send({ token, id: _id, user_name, adminId, agentId, role, active });
+      const token = jwt.sign(
+        {
+          id: _id,
+          user_name,
+          password,
+          adminId,
+          agentId,
+          role,
+          active,
+          balance,
+        },
+        "secret123",
+        { expiresIn: "1h" }
+      );
+      return res.send({
+        token,
+        id: _id,
+        user_name,
+        adminId,
+        agentId,
+        role,
+        active,
+      });
     } else {
       return res.status(401).send({ password: "Worng password" });
     }
