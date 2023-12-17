54e4e10826ba49282f073e79dfdf55d212511a03
Rajan chavda <www.chavdarajan@gmail.com>
2023-12-17 14:24:10 +0530

    added refresh jwt token

--- app/middleware/auth.middleware.js
+++ app/middleware/auth.middleware.js
@@ -1,20 +1,32 @@
 import jwt from "jsonwebtoken";
 
 export const jwtTokenValidate = async (req, res, next) => {
-  const authToken = req.headers.authorization;
-  if (authToken) {
-    try {
-      const token = authToken.slice(7);
-      const decode = jwt.verify(token, "secret123");
-      if (decode) {
-        next();
-      }
-    } catch (err) {
-      return res
-        .status(401)
-        .send("Access is denied due to invalid credentials.");
+  const authToken = req.headers?.authorization;
+  const refreshToken = req.cookies?.refreshToken;
+  if (!authToken && !refreshToken) {
+    return res.status(401).send("Access Denied. No token provided.");
+  }
+  try {
+    const token = authToken.slice(7);
+    const decode = jwt.verify(token, "mtSecret007");
+    if (decode) {
+      next();
     }
-  } else {
-    return res.status(401).send("Please provide valid credentials");
+  } catch (Err) {
+    if (!refreshToken) {
+      return res.status(401).send("Access Denied. No refresh token provided.");
+    }
+    try {
+      const decoded = jwt.verify(refreshToken, "mtRefreshSecret007");
+      const accessToken = jwt.sign(decoded, "mtSecret007", { expiresIn: "1h" });
+      return res
+        .cookie("refreshToken", refreshToken, {
+          httpOnly: true,
+          sameSite: "strict",
+        })
+        .header("Authorization", accessToken)
+        .send(decoded);
+    } catch (err) {}
+    return res.status(401).send("Access is denied due to invalid credentials.");
   }
 };
