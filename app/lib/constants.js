ed0c323826564e691b24d194ed764f5c2f965f25
Rajan chavda <www.chavdarajan@gmail.com>
2024-01-06 15:25:17 +0530

    change static times

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -1,76 +1,77 @@
-function setTime(h, m, s) {
-  return new Date().setHours(h, m, s);
-}
+// function setTime(h, m, s) {
+//   return new Date().setHours(h, m, s);
+// }
+// setTime(23, 55, 0)
 
 export const marketNames = [
   {
     name: "TIME BAZAR",
-    openTime: setTime(12, 55, 0),
-    closeTime: setTime(13, 55, 0),
+    openTime: 1704525900743,
+    closeTime: 1704529500081,
   },
   {
     name: "MADHUR DAY",
-    openTime: setTime(13, 20, 0),
-    closeTime: setTime(14, 20, 0),
+    openTime: 1704527400936,
+    closeTime: 1704531000104,
   },
   {
     name: "MADHUR NIGHT",
-    openTime: setTime(20, 25, 0),
-    closeTime: setTime(22, 25, 0),
+    openTime: 1704552900711,
+    closeTime: 1704560100545,
   },
   {
     name: "MILAN DAY",
-    openTime: setTime(14, 55, 0),
-    closeTime: setTime(16, 55, 0),
+    openTime: 1704533100398,
+    closeTime: 1704540300276,
   },
   {
     name: "MILAN NIGHT",
-    openTime: setTime(20, 55, 0),
-    closeTime: setTime(22, 55, 0),
+    openTime: 1704554700750,
+    closeTime: 1704561900012,
   },
   {
     name: "SRIDEVI",
-    openTime: setTime(11, 25, 0),
-    closeTime: setTime(12, 25, 0),
+    openTime: 1704520500179,
+    closeTime: 1704524100444,
   },
   {
     name: "SRIDEVI NIGHT",
-    openTime: setTime(19, 0, 0),
-    closeTime: setTime(20, 0, 0),
+    openTime: 1704547800197,
+    closeTime: 1704551400367,
   },
   {
     name: "RAJDHANI DAY",
-    openTime: setTime(14, 55, 0),
-    closeTime: setTime(16, 50, 0),
+    openTime: 1704551400367,
+    closeTime: 1704540000386,
   },
   {
     name: "RAJDHANI NIGHT",
-    openTime: setTime(21, 25, 0),
-    closeTime: setTime(23, 30, 0),
+    openTime: 1704556500058,
+    closeTime: 1704564000199,
   },
   {
     name: "SUPREME DAY",
-    openTime: setTime(15, 30, 0),
-    closeTime: setTime(17, 30, 0),
+    openTime: 1704535200803,
+    closeTime: 1704542400862,
   },
   {
     name: "SUPREME NIGHT",
-    openTime: setTime(20, 40, 0),
-    closeTime: setTime(22, 40, 0),
+    openTime: 1704553800899,
+    closeTime: 1704561000684,
   },
   {
     name: "KALYAN",
-    openTime: setTime(15, 50, 0),
-    closeTime: setTime(17, 50, 0),
+    openTime: 1704536400799,
+    closeTime: 1704543600859,
   },
   {
     name: "KALYAN NIGHT",
-    openTime: setTime(21, 25, 0),
-    closeTime: setTime(23, 30, 0),
+    openTime: 1704556500009,
+    closeTime: 1704564000865,
   },
   {
     name: "MAIN BAZAR",
-    openTime: setTime(21, 25, 0),
-    closeTime: setTime(23, 55, 0),
+    openTime: 1704556500852,
+    closeTime: 1704565500896,
   },
 ];
