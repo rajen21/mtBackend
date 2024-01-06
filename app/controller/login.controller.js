829b5014c158769711a2d75869789252a215715c
Rajan chavda <www.chavdarajan@gmail.com>
2024-01-06 13:43:03 +0530

    add changes

--- app/controller/login.controller.js
+++ app/controller/login.controller.js
@@ -12,9 +12,8 @@ export async function loginUser(req, res) {
       _id,
       // user_name,
       name,
-      adminId,
+      // adminId,
       phone,
-      agentId,
       role,
       active,
     } = await User.findOne({ phone: req.body.phone });
@@ -26,27 +25,27 @@ export async function loginUser(req, res) {
       });
     }
 
-    let agent;
-    let admin;
+    // let agent;
+    // let admin;
 
-    if (role === "user") {
-      agent = await User.findById(agentId);
-      if (!agent.active) {
-        return res.status(403).send({
-          inactivity:
-            "The administrator account is currently inactive. Please contact support for assistance.",
-        });
-      }
-    }
-    if (role !== "admin") {
-      admin = await User.findById(adminId);
-      if (!admin.active) {
-        return res.status(403).send({
-          inactivity:
-            "The server is currently undergoing maintenance. We apologize for any inconvenience. Please try again later.",
-        });
-      }
-    }
+    // if (role === "user") {
+    //   agent = await User.findById(agentId);
+    //   if (!agent.active) {
+    //     return res.status(403).send({
+    //       inactivity:
+    //         "The administrator account is currently inactive. Please contact support for assistance.",
+    //     });
+    //   }
+    // }
+    // if (role !== "admin") {
+    //   admin = await User.findById(adminId);
+    //   if (!admin.active) {
+    //     return res.status(403).send({
+    //       inactivity:
+    //         "The server is currently undergoing maintenance. We apologize for any inconvenience. Please try again later.",
+    //     });
+    //   }
+    // }
 
     const isPasswordValid = await bcrypt.compare(req.body.password, password);
     if (isPasswordValid) {
@@ -57,8 +56,6 @@ export async function loginUser(req, res) {
           phone,
           name,
           password,
-          adminId,
-          agentId,
           role,
           active,
           balance,
@@ -66,20 +63,6 @@ export async function loginUser(req, res) {
         "mtSecret007"
         // { expiresIn: "1h" }
       );
-      // const refreshToken = jwt.sign(
-      //   {
-      //     id: _id,
-      //     user_name,
-      //     password,
-      //     adminId,
-      //     agentId,
-      //     role,
-      //     active,
-      //     balance,
-      //   },
-      //   "mtSecret007",
-      //   // { expiresIn: "1d" }
-      // );
 
       return (
         res
@@ -93,8 +76,6 @@ export async function loginUser(req, res) {
             // user_name,
             phone,
             name,
-            adminId,
-            agentId,
             role,
             active,
           })
